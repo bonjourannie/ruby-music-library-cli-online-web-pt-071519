d1e4fd0f945998d72801443c94f5c38ce6115354
Annie Truex <artruex@gmail.com>
2019-08-25 21:48:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,12 @@ class Song
   end
   
   def self.new_from_filename 
-    
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
   end
   
   
