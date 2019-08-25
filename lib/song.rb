fdfe4beb72fa91a4eb115ccfe91c73649e80b986
Annie Truex <artruex@gmail.com>
2019-08-25 21:59:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,15 @@ class Song
     @@all << self 
   end
   
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.create(song_name, artist, genre)
+  end
+  
   def self.create(name)
     song = new(name)
     song.save
@@ -55,14 +64,7 @@ class Song
     self.new(song_name, artist, genre)
   end
   
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
 
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.create(song_name, artist, genre)
-  end
   
   
 end
