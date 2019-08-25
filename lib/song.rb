52bd281d4b8e5b2283e1db6a0cd72cfa65ed1536
Annie Truex <artruex@gmail.com>
2019-08-25 21:00:52 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song 
+  
+  attr_accessor :name, :genre, :artist 
+  
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    self.artist = artist if artist
+    self.genre = genre if genre 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end 
+    
+  def genre=(genre)
+    @genre = genre
+     genre.songs << self unless genre.songs.include?(self)
+  end
+  
+  def self.destroy_all 
+    @@all.clear
+  end
+  
+  def save 
+    @@all << self 
+  end
+  
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect{|s| s.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+  
+  
+end
